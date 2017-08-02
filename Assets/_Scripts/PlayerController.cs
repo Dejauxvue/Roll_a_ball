20b94289cc945668943500d7c94678e8fd80aaa9
Dejauxvue <josuabloess@gmx.de>
2017-08-02 23:13:59 +0200

    finished lection 3

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
     }
     void FixedUpdate(){
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -16,5 +23,20 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         rb.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) winText.text = "You Win";
     }
 }
